fb550e7d76a13be9accce4330ba6044a4e1c418e
REXSY <abhinavsingh.thedev@gmail.com>
2025-09-12 02:42:42 +0530

    Unordered set

---
+++ C++ STL/Containers/Set/Unordered_set.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+// It is Unique but not sorted
+void explainUSet(){
+    unordered_set<int> st;
+    // lower_bound and upper_bound function does not works, rest all functions are same
+    // it does not stores in any particular order, it has a better complexity
+    // than set in most cases, except some when collision happens
+}
+
+int main(){
+    explainUSet();
+}
