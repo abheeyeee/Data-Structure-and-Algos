c21090a9fab8ef8f463ace241d505c51de7cd0f0
REXSY <abhinavsingh.thedev@gmail.com>
2025-09-22 00:47:58 +0530

    MultiMap & unorderedmap

---
+++ C++ STL/Containers/Map/MultiMap.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+void explainMultiMap(){
+    // everything same as map, only diff is it can store multiple duplicate keys
+    // only mpp[key] cannot be used here
+}
+
+int main(){
+    explainMultiMap();
+}
---
+++ C++ STL/Containers/Map/UnorderedMap.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+void explainUnorderedMap(){ // unique and unsorted
+    // Duplicate keys are not allowed → inserting a key that exists overwrites the value.
+     // same as set and unordered_set difference.
+     // not in sorted order
+     // not have duplicate keys
+     // Print elements (order is not guaranteed)
+
+     //Average time complexity for insertion, deletion, lookup = O(1).
+     //Worst case = O(n) (if many collisions).
+}
+
+int main(){
+    explainUnorderedMap();
+}
