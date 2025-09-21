62273eeced270d92493475413bbe83489cc8169b
REXSY <abhinavsingh.thedev@gmail.com>
2025-09-22 02:48:02 +0530

    min/max

---
+++ C++ STL/max_element.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int maxi = *max_element(a,a+n); // this gives u the adddress of the max or mini and using * gives the element.
+    int mini = *min_element(a,a+n);
+}
